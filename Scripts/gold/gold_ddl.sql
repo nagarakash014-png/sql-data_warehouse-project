ec4f2344f5e4bf839cc8fc207e256d9b46290778
nagarakash014-png <nagarakash014@gmail.com>
2026-09-14 19:48:50 +0530

    Create or alter views for customer, product, and sales data
    This script creates or alters three views: dim_cust, dim_prd, and fact_sales. Each view aggregates and formats customer, product, and sales data from the underlying tables.

---
+++ Scripts/gold/gold_ddl.sql
@@ -0,0 +1,100 @@
+-- =========================================================================
+-- View 1: gold.dim_cust
+-- =========================================================================
+
+if object_id('gold.dim_cust','v') is not null
+drop view gold.dim_cust
+print'============================================='
+print'creating view 1: gold.dim_cust'
+print'============================================='
+
+go 
+
+create or alter view gold.dim_cust as
+select
+row_number()  over (order by cst_id) as cust_key,
+cst_id as cust_id,
+cst_key  as cust_number,
+first_name,
+last_name,
+case when cst_gndr in ('Female','Male') then cst_gndr 
+else coalesce(gender,'UN')
+end as gender,
+country,
+martial_status,
+bdate as birth_date
+from silver.crm_cust_info as s
+left join silver.erp_cust_az12 as z
+on s.cst_key = z.cid
+left join silver.erp_loc_a101 as l
+on s.cst_key  = l.cid
+
+go
+
+-- =========================================================================
+-- View 2: gold.dim_prd
+-- =========================================================================
+
+if object_id('gold.dim_prd','v') is not null
+drop view gold.dim_prd
+
+print'============================================='
+print'creating view 2: gold.dim_prd'
+print'============================================='
+ 
+ go 
+
+create or alter  view gold.dim_prd as 
+select
+row_number() over(order by prd_id) product_key,
+prd_id as product_id,
+prd_cust_id as categorie_id ,
+prd_key as product_number,
+prd_nm as product_name,
+prd_cost as cost,
+categorie as categorie,
+sub_cat as sub_categorie,
+prd_line as product_line,
+maintenance as  maintenance,
+prd_start_dt as start_date,
+prd_end_dt as end_date
+from silver.crm_prd_info as s
+left join  silver.erp_px_cat_giv2 as g
+on s.prd_cust_id = g.id
+where prd_end_dt is null
+
+go
+
+-- =========================================================================
+-- View 3: gold.fact_sales
+-- =========================================================================
+
+if object_id('gold.fact_sales','v') is not null
+drop view gold.fact_sales
+
+print'============================================='
+print'creating view 3: gold.fact_sales'
+print'============================================='
+
+go
+
+create or alter view gold.fact_sales as
+select 
+sls_ord_num as order_number,
+p.product_key,
+cust_key,
+sls_ord_dt as order_date,
+sls_ship_dt as ship_date,
+sls_due_dt as due_date,
+sls_sales as sales,
+sls_quantity as quantity,
+sls_price as price
+from silver.crm_sales_details as s
+left join gold.dim_prd as p
+on s.sls_prd_key = p.product_number
+left join  gold.dim_cust as g
+on s.sls_cust_id = g.cust_id
+
+go
+
+
